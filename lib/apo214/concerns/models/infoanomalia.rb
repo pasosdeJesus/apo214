6cba8ec9edd8a6c899242c2f2557536564588c99
alejocruzrcc <alejocruzzz@gmail.com>
2021-02-01 16:25:55 -0500

    funciona info anomalias

--- lib/apo214/concerns/models/infoanomalia.rb
+++ lib/apo214/concerns/models/infoanomalia.rb
@@ -7,7 +7,8 @@ module Apo214
         extend ActiveSupport::Concern
 
         included do
-
+          validates :latitud, :inclusion => {in: -90..90, message: 'El valor de la latitud debe ser entre -90 y 90'}
+          validates :longitud, :inclusion => {in: -180..180, message: 'El valor de longitud debe ser entre -180 y 180'}
         end
 
       end
