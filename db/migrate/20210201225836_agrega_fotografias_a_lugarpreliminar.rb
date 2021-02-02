4a02abb2886932c69baecb72b33e7167d0e0d19a
alejocruzrcc <alejocruzzz@gmail.com>
2021-02-02 03:15:34 -0500

    cambia forma de adjunta

--- db/migrate/20210201225836_agrega_fotografias_a_lugarpreliminar.rb
+++ db/migrate/20210201225836_agrega_fotografias_a_lugarpreliminar.rb
@@ -1,12 +1,12 @@
 class AgregaFotografiasALugarpreliminar < ActiveRecord::Migration[6.0]
   def up
     create_table :apo214_infofoto do |t|
-      t.binary :fotografia, limit: 100
       t.string :descripcion, limit: 5000
       t.date :fecha
       t.timestamp :created_at, null: false
       t.timestamp :updated_at, null: false
     end
+    add_attachment :apo214_infofoto, :fotografia
     create_table :apo214_listainfofoto do |t|
       t.integer :lugarpreliminar_id, null: false
       t.integer :infofoto_id, null: false
@@ -17,6 +17,7 @@ class AgregaFotografiasALugarpreliminar < ActiveRecord::Migration[6.0]
 
   def down
     drop_table :apo214_listainfofoto
+    remove_attachment :apo214_infofoto, :fotografia
     drop_table :apo214_infofoto
   end
 end
