953c3b663f2c28dfdd78124d0e62eb248da76ca4
alejocruzrcc <alejocruzzz@gmail.com>
2021-02-07 19:28:56 -0500

    arregla controlador, arregla vista y mejora migración porque se cambió

--- db/migrate/20210130043950_agrega_perfilestratigrafico_a_lugarpreliminar.rb
+++ db/migrate/20210130043950_agrega_perfilestratigrafico_a_lugarpreliminar.rb
@@ -1,6 +1,10 @@
 class AgregaPerfilestratigraficoALugarpreliminar < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :apo214_lugarpreliminar, :perfilestratigrafico, :string, 
+      limit:5000
+  end
+  def down
+    remove_column :apo214_lugarpreliminar, :perfilestratigrafico, :string, 
       limit:5000
   end
 end
