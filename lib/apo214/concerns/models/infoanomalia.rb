8c0653d90fc9a8725cc17b1ab5fdf7db6bbe2d5a
alejocruzrcc <alejocruzzz@gmail.com>
2021-04-06 23:39:19 -0500

    comienza vista geoloc

--- lib/apo214/concerns/models/infoanomalia.rb
+++ lib/apo214/concerns/models/infoanomalia.rb
@@ -11,6 +11,8 @@ module Apo214
           include Sip::Localizacion
           flotante_localizado :latitud 
           flotante_localizado :longitud
+          attr_accessor :latitud_sirgas
+          attr_accessor :longitud_sirgas
           belongs_to :sip_anexo, class_name: 'Sip::Anexo', validate: true,
             foreign_key: 'anexo_id'
           accepts_nested_attributes_for :sip_anexo, reject_if: :all_blank
