3801bf6256d992bca952ef569bd1053e88ac3ac7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-10-11 05:21:25 -0500

    Actualiza

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ require 'rails/test_help'
 require 'rake'
 
 Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
-Dummy::Application.load_tasks # providing your application name is 'sample'
+Apo214Demo::Application.load_tasks # providing your application name is 'sample'
 
 class ActiveSupport::TestCase
   #fixtures :all
