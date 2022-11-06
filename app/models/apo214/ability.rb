169c00d89cd180234723106255c8f67878e7629c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-06 14:52:13 -0500

    Actualiza

--- app/models/apo214/ability.rb
+++ app/models/apo214/ability.rb
@@ -25,6 +25,35 @@ module Apo214
           return r
     end
 
+    NOBASICAS_INDSEQID = [
+      ['Apo214', 'asisreconocimiento'], 
+      ['Apo214', 'evaluacionriesgo'], 
+      ['Apo214', 'infoanomalia'], 
+      ['Apo214', 'infoanomalialugar'], 
+      ['Apo214', 'listaanexo'], 
+      ['Apo214', 'listadepositados'], 
+      ['Apo214', 'listaevariesgo'], 
+      ['Apo214', 'listainfofoto'], 
+      ['Apo214', 'listapersofuentes'], 
+      ['Apo214', 'listasuelo'], 
+      ['Apo214', 'lugarpreliminar'], 
+      ['Apo214', 'propietario'], 
+      ['Apo214', 'suelo']
+    ]
+
+    # Tablas no básicas pero que tienen índice *_seq_id
+    def nobasicas_indice_seq_con_id
+      Sip::Ability::NOBASICAS_INDSEQID +
+        Mr519Gen::Ability::NOBASICAS_INDSEQID +
+        Heb412Gen::Ability::NOBASICAS_INDSEQID +
+        Sivel2Gen::Ability::NOBASICAS_INDSEQID +
+        Apo214::Ability::NOBASICAS_INDSEQID
+    end
+
+    def nobasicas_indice_seq_con_id
+      NOBASICAS_INDSEQID
+    end
+
     # Establece autorizaciones con CanCanCan
     def initialize_apo214(usuario = nil)
       initialize_sivel2_gen(usuario)
