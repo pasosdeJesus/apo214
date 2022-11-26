8f0e283b416a4581abd58f3a4409d519104bae58
Luis Alejandro Cruz <alejocruzzz@gmail.com>
2022-11-25 19:45:15 -0500

    previene inyeccion sql

--- lib/apo214/concerns/controllers/lugarespreliminares_controller.rb
+++ lib/apo214/concerns/controllers/lugarespreliminares_controller.rb
@@ -241,14 +241,15 @@ module Apo214
             when 1
               lat_wgs84_flot = params[:lat_wgs84_flot]
               lon_wgs84_flot = params[:lon_wgs84_flot]
-              punto = 'POINT('+ lon_wgs84_flot + " " + lat_wgs84_flot + ')'
-              consl_sirgas = 'SELECT st_astext(st_transform( st_geometryfromtext(\''+ punto +'\', 4326 ), 3116) );'
+              conn = ActiveRecord::Base.connection
+              punto = conn.quote('POINT('+ lon_wgs84_flot + " " + lat_wgs84_flot + ')')
+              consl_sirgas = "SELECT st_astext(st_transform( st_geometryfromtext('#{punto}', 4326 ), 3116) );"
               consl_on = 'SELECT st_astext(st_transform( st_geometryfromtext( 
               \''+ punto +'\', 4326 ), \'+proj=tmerc +ellps=GRS80 +lat_0=4 
               +lon_0=-73 +k=0.9992 +x_0=5000000 +y_0=2000000 +units=m +no_defs\'
               ::text) );'
-              resultado_sirgas = ActiveRecord::Base.connection.select_all consl_sirgas
-              resultado_on = ActiveRecord::Base.connection.select_all consl_on
+              resultado_sirgas = conn.execute(consl_sirgas)
+              resultado_on = conn.execute(consl_on)
               if resultado_sirgas.rows[0][0] == "POINT(inf inf)"
                 mensaje_error = "Valor ingresado no permitido"
               else
@@ -265,9 +266,9 @@ module Apo214
             when 3
               lat_sirgas = params[:lat_sirgas]
               lon_sirgas = params[:lon_sirgas]
-              punto = 'POINT('+ lon_sirgas + " " + lat_sirgas + ')'
-              consl_wgs84 = 'SELECT st_astext(st_transform( st_geometryfromtext(\''+ punto +'\', 3116 ), 4326) );'
-              resultado_wgs84 = ActiveRecord::Base.connection.select_all consl_wgs84
+              punto = conn.quote('POINT('+ lon_sirgas + " " + lat_sirgas + ')')
+              consl_wgs84 = "SELECT st_astext(st_transform( st_geometryfromtext('#{punto}', 3116 ), 4326) );"
+              resultado_wgs84 = conn.execute(consl_wgs84)
               punto_wgs84 = resultado_wgs84.rows[0][0]
               if punto_wgs84 == "POINT(inf inf)"
                 mensaje_error = "Valor ingresado no permitido"
@@ -276,11 +277,11 @@ module Apo214
                 \''+ punto_wgs84 +'\', 4326 ), \'+proj=tmerc +ellps=GRS80 +lat_0=4 
                 +lon_0=-73 +k=0.9992 +x_0=5000000 +y_0=2000000 +units=m +no_defs\'
                 ::text) );'
-                resultado_on = ActiveRecord::Base.connection.select_all consl_on
+                resultado_on = conn.execute(consl_on)
                 coor_wgs84 = resultado_wgs84.rows[0][0].slice(6..-2).split(" ")
                 coor_on = resultado_on.rows[0][0].slice(6..-2).split(" ")
                 consl_gms= 'SELECT (ST_AsLatLonText(\''+ punto_wgs84 +'\', \'D°M\'\'S.SSS\'\'C\'));'
-                resultado_gms = ActiveRecord::Base.connection.select_all consl_gms
+                resultado_gms = conn.execute(consl_gms)
                 coor_gms = resultado_gms.rows[0][0]
                 coor_sirgas = punto.slice(6..-2).split(" ")
               end
