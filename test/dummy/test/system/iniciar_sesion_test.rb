c3611e22e417370f0f90472b42ec9c427fd66b40
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-26 19:26:54 -0500

    Actualiza

--- test/dummy/test/system/iniciar_sesion_test.rb
+++ test/dummy/test/system/iniciar_sesion_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class IniciarSesionTest < ApplicationSystemTestCase
 
   test "iniciar sesión" do
-    Msip::CapybaraHelper.iniciar_sesion(self, root_path, 'msip', 'msip')
+    Msip::CapybaraHelper.iniciar_sesion(self, root_path, 'sivel2', 'sivel2')
   end
 
 end
