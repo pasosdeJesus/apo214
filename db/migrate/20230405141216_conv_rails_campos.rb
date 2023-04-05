a390486d6c904fe4bb5fe7ec9c940b3eb393e384
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-05 09:39:40 -0500

    convenciones tipo rails para campos

---
+++ db/migrate/20230405141216_conv_rails_campos.rb
@@ -0,0 +1,8 @@
+class ConvRailsCampos < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :apo214_lugarpreliminar, :id_persona, :persona_id
+
+    rename_column :apo214_propietario, :id_persona, :persona_id
+    rename_column :apo214_propietario, :id_lugarpreliminar, :lugarpreliminar_id
+  end
+end
