1f375c0276ba313b25ebcdd8bec17e496ce2157b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-08-21 09:31:58 -0500

    Actualiza inf

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link application.js
 //= link logo.jpg
 //= link spinnern.gif
-//= link sivel2_gen/mapaosm.js
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
